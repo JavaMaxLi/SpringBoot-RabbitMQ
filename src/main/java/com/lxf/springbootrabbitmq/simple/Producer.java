9f2de0c27ab68b43c94cbf9ff13e9792bba049cf
LLLLLL <913577706@qq.com>
2022-01-22 15:46:57 +0800

    direct模式

--- src/main/java/com/lxf/springbootrabbitmq/simple/Producer.java
+++ src/main/java/com/lxf/springbootrabbitmq/simple/Producer.java
@@ -18,8 +18,8 @@ public class Producer {
         connectionFactory.setUsername("admin");
         connectionFactory.setPassword("admin123");
         connectionFactory.setVirtualHost("/");
-        String content = "Hello RabbitMQ,I am Learn";
-        String queueName = "queue1";
+        String content = "Hello RabbitMQ,I'm queue2";
+        String queueName = "queue2";
         Connection connection = null;
         Channel channel = null;
         try{
@@ -33,9 +33,28 @@ public class Producer {
             // @param3 排他性
             // @param4 是否自动删除，最后一个消费者消费完是否删除队列
             // @param5 附加参数
-            channel.queueDeclare(queueName,false,false,false,null);
+           channel.queueDeclare(queueName,true,false,false,null);
 
-            channel.basicPublish("",queueName,MessageProperties.PERSISTENT_TEXT_PLAIN,content.getBytes());
+            String exchangeName = "direct_order";
+            String exchangeType = "direct";
+            //定义交换机
+            //@param1交换机名称
+            //@param2交换机类型
+            //@param3交换机是否持久化  持久化服务器重启不会丢失
+    /*         channel.exchangeDeclare(exchangeName,exchangeType,true);
+            //声明队列
+            channel.queueDeclare("queue_order_01",true,false,false,null);
+            channel.queueDeclare("queue_order_02",true,false,false,null);
+            channel.queueDeclare("queue_order_03",true,false,false,null);
+            //绑定队列
+            channel.queueBind("queue_order_01",exchangeName,"course");
+            channel.queueBind("queue_order_02",exchangeName,"user");
+            channel.queueBind("queue_order_03",exchangeName,"work");
+*/
+            //使用默认交换机
+            channel.basicPublish(exchangeName,"course",MessageProperties.PERSISTENT_TEXT_PLAIN,"hello,queue_order course".getBytes());
+            channel.basicPublish(exchangeName,"user",MessageProperties.PERSISTENT_TEXT_PLAIN,"hello,queue_order user".getBytes());
+      /*      channel.basicPublish(exchangeName,"work",MessageProperties.PERSISTENT_TEXT_PLAIN,"hello,queue_order work".getBytes());*/
         }catch (Exception e) {
             e.printStackTrace();
         }finally {
